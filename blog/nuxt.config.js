852c2f22fee467720042aed34cc6dc911086b16f
Brandon Kucybala <bkucybala@gmail.com>
2022-12-31 17:39:38 -0500

    yep

--- blog/nuxt.config.js
+++ blog/nuxt.config.js
@@ -49,7 +49,6 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
-    proxyHeaders: false,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
