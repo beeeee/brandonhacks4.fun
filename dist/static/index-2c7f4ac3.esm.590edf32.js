4c304c2f8f17b9bcc91a59b5fe29101fa2bedf45
Brandon Kucybala <bkucybala@gmail.com>
2022-12-31 18:34:35 -0500

    package

--- dist/static/index-2c7f4ac3.esm.590edf32.js
+++
@@ -1,1 +0,0 @@
-import{a as t,D as m}from"./index-71beeb8c.esm.8ae12313.js";import"./sanity.b1d57b43.js";import"./index.625891f7.js";export{t as DocumentPaneProvider,m as default};
---
+++ dist/static/index-2c7f4ac3.esm.7b9438b3.js
@@ -0,0 +1,1 @@
+import{a as t,D as m}from"./index-71beeb8c.esm.f6ce90fe.js";import"./sanity.d83ab1f5.js";import"./index.2e290501.js";export{t as DocumentPaneProvider,m as default};
